17bff2415996e3ba53dc91d3197138c66ed3b2a2
shakedva <shakediii123@gmail.com>
2023-12-31 17:41:23 +0200

    Debounce api call for autocomplete

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 const autocompleteAddress = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete'
 const apiKey = 'aID1uGelP7d2tREmiHtpwKlPUNty1Be6'
 
@@ -6,37 +6,49 @@ export default function SearchBar({ onLocationClicked }) {
     const [location, setLocation] = useState('');
     const [cities, setCities] = useState([]);
 
+    useEffect(() => {
+        const getData = setTimeout(() => {
+            if (location === '' || cities.includes(location)) {
+                return;
+            }
+            fetch(`${autocompleteAddress}?apikey=${apiKey}&q=${location}`)
+                .then(res => res.json())
+                .then(json => {
+                    console.log(json)
+
+                    const suggestions = []
+                    json.forEach(city => {
+                        console.log(`debounce: ${city.LocalizedName}, ${city.Country.ID}`)
+                        const cityData = `${city.LocalizedName}, ${city.Country.ID}`
+                        suggestions.push(cityData)
+                    });
+                    setCities(suggestions);
+
+                }).catch(function () {
+                    console.log(`servers are not available right now`)
+                })
+        }, 5000);
+
+        return () => clearTimeout(getData);
+    }, [location]);
+
     function handleLocationChange(event) {
-        const city = event.target.value;
+        let city = event.target.value;
+        city = city.trim();
         setLocation(city);
         if (cities.includes(city)) {
-            onLocationClicked(city)
+            onLocationClicked(city);
         }
-    }
-    function handleSubmit() {
-        console.log(location);
-
-        fetch(`${autocompleteAddress}?apikey=${apiKey}&q=${location}`)
-            .then(res => res.json())
-            .then(json => {
-                console.log(json)
-
-                const suggestions = []
-                json.forEach(city => {
-                    console.log(`${city.LocalizedName}, ${city.Country.ID}`)
-                    const cityData = `${city.LocalizedName}, ${city.Country.ID}`
-                    suggestions.push(cityData)
-                });
-                setCities(suggestions);
-
-            }).catch(function () {
-                console.log(`servers are not available right now`)
-            })
     }
     return (
         <div className="d-flex justify-content-center">
             <div className="col-5">
-                <input className="form-control" type="text" list="cities" value={location} onChange={handleLocationChange} />
+                <input
+                    className="form-control"
+                    type="text" list="cities"
+                    value={location}
+                    onChange={handleLocationChange}
+                />
                 <datalist id="cities">
                     {cities.map(city => {
                         return (
@@ -49,9 +61,6 @@ export default function SearchBar({ onLocationClicked }) {
                         )
                     })}
                 </datalist>
-            </div>
-            <div>
-                <button className="btn btn-outline-success" onClick={handleSubmit}>Search</button>
             </div>
         </div>
     )
