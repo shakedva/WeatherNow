39b17e9fef437873ef8ceabd94a1c73c819bab51
shakedva <shakediii123@gmail.com>
2024-01-01 17:35:37 +0200

    Remove favorite location

--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -11,7 +11,7 @@ export default function LocationDetails({ location }) {
     const [details, setDetails] = useState(DEFAULT_DETAILS);
     const [savedLocation, setSavedLocation] = useState(false);
 
-    const { addToFavorites, favorites } = useContext(FavoriteLocationsContext);
+    const { addToFavorites, removeFromFavorites, favorites } = useContext(FavoriteLocationsContext);
 
     // Check if the location is saved as favorite
     useEffect(() => {
@@ -36,8 +36,14 @@ export default function LocationDetails({ location }) {
     }, [location]);
 
     function handleSaveLocation() {
-        addToFavorites(location);
+        const isSaved = favorites.some(favLocation => favLocation.key === location.key);
+        if (isSaved) {
+            removeFromFavorites(location)
+        } else {
+            addToFavorites(location);
+        }
     }
+    
     let bookmark = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bookmark" viewBox="0 0 16 16" onClick={handleSaveLocation}>
         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" />
     </svg>
--- src/favorites/FavoriteLocationsContext.jsx
+++ src/favorites/FavoriteLocationsContext.jsx
@@ -3,17 +3,27 @@ import { createContext, useState, useReducer } from 'react'
 export const FavoriteLocationsContext = createContext({
     favorites: [],
     addToFavorites: () => { },
+    removeFromFavorites: () => { },
 });
 function favoriteLocationsReducer(state, action) {
     if(action.type === 'ADD-FAVORITE'){
-        const updatedFavorites = [...state.favorites]; 
-        updatedFavorites.push({
-            key: action.payload.key,
-            localizedName: action.payload.localizedName,
-        });
+        const updatedFavorites = [...state.favorites];
+        if(!updatedFavorites.some(favLocation => favLocation.key === action.payload.key)) {
+            updatedFavorites.push({
+                key: action.payload.key,
+                localizedName: action.payload.localizedName,
+            });
+        }        
         return {
             favorites: updatedFavorites,
         };
+    }
+    if(action.type === 'REMOVE-FAVORITE'){
+        const updatedFavorites = state.favorites.filter(favLocation => favLocation.key !== action.payload.key);
+        return {
+            favorites: updatedFavorites,
+        };
+
     }
 }
 
@@ -30,9 +40,17 @@ export default function FavoriteLocationsProvider({ children }) {
             payload: location
         });
     }
+    function handleRemoveFromFavorites(location) {
+        favoriteLocationsDispatch({
+            type: 'REMOVE-FAVORITE',
+            payload: location
+        });
+    }
+
     const ctxValue = {
         favorites: favoriteLocationsState.favorites,
         addToFavorites: handleAddToFavorites,
+        removeFromFavorites: handleRemoveFromFavorites,
     }
     return(
         <FavoriteLocationsContext.Provider value={ctxValue}>
