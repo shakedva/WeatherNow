e8af9d3d4d9a2be7e95b8bfe81a0e30919ff6aed
shakedva <shakediii123@gmail.com>
2024-01-02 18:38:29 +0200

    Theme context

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import Favorites from './components/Favorites.jsx';
 import Home from './components/Home.jsx';
 import FavoriteLocationsProvider from "./contexts/FavoriteLocationsContext.jsx";
 import TemperatureProvider from "./contexts/TemperatureContext.jsx";
+import ThemeProvider from "./contexts/ThemeContext.jsx";
 import { clearStorageAfterTtlMinutes } from "./util.js";
 import './App.css';
+
 
 function App() {
   clearStorageAfterTtlMinutes();
   return (
-    <FavoriteLocationsProvider>
-      <TemperatureProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Header />}>
-              <Route index element={<Home />} />
-              <Route path="favorites" element={<Favorites />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </TemperatureProvider>
-    </FavoriteLocationsProvider>
+    <ThemeProvider>
+      <FavoriteLocationsProvider>
+        <TemperatureProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Header />}>
+                <Route index element={<Home />} />
+                <Route path="favorites" element={<Favorites />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </TemperatureProvider>
+      </FavoriteLocationsProvider>
+    </ThemeProvider>
   );
 }
 
