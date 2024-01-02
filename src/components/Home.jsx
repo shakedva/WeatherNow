8028fedbbdd5f0c77027b32f5a4ab7158af9aae6
shakedva <shakediii123@gmail.com>
2024-01-02 12:31:21 +0200

    Default location via geolocation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,23 +2,51 @@ import { useState, useEffect } from "react"
 import { useLocation } from 'react-router-dom';
 import SearchBar from "./SearchBar.jsx"
 import Forecast from "./Forecast.jsx"
+import { geopositionAddress, apiKey } from "../util.js";
 
 const DEFAULT_LOCATION = {
     key: '215854',
     localizedName: 'Tel Aviv',
     country: 'Israel',
 }
+
 export default function Home({location = DEFAULT_LOCATION}) {
     const { state } = useLocation();
     const [selectedLocation, setSelectedLocation] = useState(location)
     function handleLocationClicked(location){
         setSelectedLocation(location);
     }
+
     useEffect(() => {
+        function getUserCurrentPosition() {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+                fetch(`${geopositionAddress}?apikey=${apiKey}&q=${lat}%2C${lon}`)
+                .then(res => res.json())
+                .then(json => {
+                    const currentLocation = {
+                        key: json.Key,
+                        localizedName: json.LocalizedName,
+                        country: json.Country.LocalizedName,
+                    };
+                    setSelectedLocation(currentLocation);
+                }).catch(function () {
+                    console.log(`servers are not available right now`)
+                    setSelectedLocation(DEFAULT_LOCATION);
+                })
+            }, (error) => {
+                console.warn(`ERROR(${error.code}): ${error.message}`);
+                setSelectedLocation(DEFAULT_LOCATION);
+            });
+        }
         if(state) {
             setSelectedLocation(state.location);
+        } else {
+            getUserCurrentPosition();
         }
-    }, [location]);
+    }
+    ,[state]);
 
     return (
         <div id="home">
--- src/util.js
+++ src/util.js
@@ -1,8 +1,9 @@
-export const apiKey = 'aID1uGelP7d2tREmiHtpwKlPUNty1Be6';
+export const apiKey = 'x5SN5T7HFN8EDHhiIYF4GJ6HbmvESGjC';
 export const currentConditionsAddress = 'http://dataservice.accuweather.com/currentconditions/v1/';
 export const autocompleteAddress = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete';
 export const fiveDayForecastAddress = 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/';
- 
+export const geopositionAddress = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search'
+
 export function convertISO8601ToDayOfTheWeek(originalDate) {
     const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const date = new Date(originalDate);
